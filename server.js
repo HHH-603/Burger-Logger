c0f1ec37570a9200c8889b0fdfc5efc84035bb11
Henry Hamilton Holmes <hholmes726@gmail.com>
2020-12-12 22:11:34 -0500

    Added  links next to each non-devoured burger item to bring users to a single-burger update page if they want to update a burger's name.

--- server.js
+++ server.js
@@ -18,6 +18,22 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+app.put("/api/burgers/:id", function(req, res) {
+  createConnection.query(
+    "UPDATE burgers SET burger_name = ?, id = ?",
+    [req.body.burger_name, req.params.id],
+    function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+      else if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  )
+});
+
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
