f2d198781893d9c442beba3d4754877cb7e769af
Henry Hamilton Holmes <hholmes726@gmail.com>
2020-12-13 00:01:09 -0500

    FINAL round of edits.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -8,35 +8,30 @@ $(function () {
             devoured: true
         };
 
-        // Send the PUT request.
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newDevouredState
         }).then(
             function () {
                 console.log("changed devoured to", newDevoured);
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
 
     $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
         var newDevoured = {
             burger_name: $("#ca").val().trim(),
             devoured: $("[name=devoured]:checked").val().trim()        };
 
-        // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newDevoured
         }).then(
             function () {
                 console.log("created new burger");
-                // Reload the page to get the updated list
                 location.reload();
             }
         );
